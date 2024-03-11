fa486e453f5c13276881f3f2790658cbcfde2594
julia8956 <minji6654@gmail.com>
2024-03-11 19:12:59 +0900

    style: add formatters

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx'],
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  arrowParens: 'always',
+};
