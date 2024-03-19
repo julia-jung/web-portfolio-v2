aab8f94f6284a143684dfe2a30a3201ef79712c9
julia8956 <minji6654@gmail.com>
2024-03-19 22:47:35 +0900

    chore: added portfolio project url

--- lib/data.ts
+++ lib/data.ts
@@ -93,6 +93,6 @@ export const projects: Project[] = [
     thumbnail: '/images/portfolio.png',
     stack: ['Frontend', 'Typescript', 'React', 'Next.js', 'Tailwind CSS'],
     github: 'https://github.com/julia-jung/web-portfolio-v2',
-    url: '',
+    url: 'https://minji-jung.vercel.app/',
   },
 ];
