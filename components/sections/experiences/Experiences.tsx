63fbf1cf088a1a96d8795a4d9162693dba77623e
julia8956 <minji6654@gmail.com>
2024-04-11 21:33:48 +0900

    fix: updated cv

--- components/sections/experiences/Experiences.tsx
+++ components/sections/experiences/Experiences.tsx
@@ -11,7 +11,7 @@ export default function Experience() {
           <WorkCard {...experience} />
         </Link>
       ))}
-      <Link href="docs/cv_minjijung.pdf" target="_blank" rel="noreferrer noopener">
+      <Link href="cv_minjijung.pdf" target="_blank" rel="noreferrer noopener">
         <div className="group flex items-center gap-1 p-4 font-semibold">
           <span className="bg-gradient-to-r from-cyan-400 to-violet-400 bg-clip-text text-lg text-transparent transition duration-500 ease-out after:block after:border-violet-400 hover:after:border-b-2">
             Check out my CV for details
